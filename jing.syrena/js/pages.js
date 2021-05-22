580970b28885a91a6a73d37ba55a70463490a962
Syrenajing <pink@YifengdeMacBook-Pro.local>
2021-05-22 00:43:42 -0700

    [so many functions]

--- jing.syrena/js/pages.js
+++ jing.syrena/js/pages.js
@@ -128,10 +128,15 @@ const AnimalProfilePage = async () => {
       params:[sessionStorage.animalId]
    }).then(async (r)=>{
       console.log("ANIMAL LOCATIONS", r.result)
+
       let map_el = await makeMap("#animal-profile-page .map");
       makeMarkers(map_el,r.result)
    });
 }
+
+
+
+
 
 const AnimalEditPage = async () => {
    let animal = await query({
